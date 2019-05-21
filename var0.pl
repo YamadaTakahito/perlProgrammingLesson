492ce75f8c758fd082e889919183213898557204
Takahito Yamada <d_p.t.a.d.fighter@icloud.com>
2019-05-21 21:43:04 +0900

    Add var1

---
+++ var0.pl
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+
+my $price = 2000;
+print("$price\n");
+
+print($price * 0.0);
+print("\n");
+
+$price = 10000;
+print("$price\n");
+
+
+my $discount_price = $price * 0.9;
+print("$discount_price\n")
